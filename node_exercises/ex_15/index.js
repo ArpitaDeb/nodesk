22c898f4e5ab02dd2709d6c8e1442f5c6ba8876f
Jesse Witow <jwitow@gmail.com>
2018-07-30 19:53:15 -0600

    a bunch of modules

---
+++ node_exercises/ex_15/index.js
@@ -0,0 +1,30 @@
+
+    const express = require('express');
+    const app = express();
+
+    let statusMSG = {
+        "status": 200,
+        "message": 'The request/response was OK? I think?'
+    };
+
+    const requestTime = (req, res, next) => {
+        let message = `Request: ${req.baseUrl} ${Date.now()}`;
+        console.log(message);
+
+        next();
+    };
+
+    app.use(requestTime);
+
+    app.get('/', function(req,res) {
+        res.json(statusMSG);
+    });
+
+    app.listen (3000, function() {
+        console.log(`Listening on port 3000`);
+    });
+
+
+
+
+  
