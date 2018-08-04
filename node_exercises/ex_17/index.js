ac15922b7a896afc45e3b15c1138e9b6e793a3b1
Jesse Witow <jwitow@gmail.com>
2018-08-03 20:27:27 -0600

    Added to ex_17, the last of pug exercises including extensions, javascript, etc.

--- node_exercises/ex_17/index.js
+++ node_exercises/ex_17/index.js
@@ -4,7 +4,10 @@ const app = express();
 app.set('view engine', 'pug')
 
 app.get('/', (req,res) => {
-    res.render('index',{ title: 'Hey', header: 'HEADER!', message: 'Hello there!'})
+    res.render('index',{ title: 'Hey', header: 'HEADER 1', message: 'This is stuff passed from the index.js \'res.render\' call.'})
 })
 
+app.get('/index2', (req,res) => {
+    res.render('index2')
+})
 app.listen(3000);
